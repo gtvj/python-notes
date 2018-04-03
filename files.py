8629f9afc4e9916c847ee4b43a5293ac376a1c83
Gwyn Jones <gwynjones@Gwyns-MacBook-Pro.local>
2018-04-03 22:01:25 +0100

    Update files example to include copying

--- files.py
+++ files.py
@@ -1,7 +1,11 @@
 def main():
-    f = open('README.md')
-    for line in f:
-        print(line.rstrip())
+    input_file = open('README.md', 'rt')  # Open the file in 'read' and 'text' mode
+    output_file = open('README-copy.md', 'wt')  # Open or create
+    for line in input_file:
+        print(line.rstrip(), file=output_file)  # Note: we're using print here but passing an output file
+        print('.', end='', flush=True)  # Printing to standard out
+    output_file.close()
+    print('\ndone')
 
 
 if __name__ == '__main__':
